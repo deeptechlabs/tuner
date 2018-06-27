382954b309cd4b37aaac92d4f6ecd66d9bda450b
Dendi Suhubdy <dendi@keatext.com>
2018-06-27 14:00:19 -0400

    setting up research plan, execution plan, LICENSE and project C++17 skeleton

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <boost/program_options.hpp>
+
+// Global headers
+//
+#include <iostream>
+#include <string>
+
+// Local headers
+#include "evolve.h"
+
+namespace
+{
+  const size_t ERROR_IN_COMMAND_LINE = 1; 
+  const size_t SUCCESS = 0; 
+  const size_t ERROR_UNHANDLED_EXCEPTION = 2; 
+
+} // namespace 
+
+int main(int argc, char** argv) 
+{
+     
+} // main 
